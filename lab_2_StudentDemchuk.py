9fb016ae2190a73f7200bb7fd72bc8a7c9357919
Денис <demchuk.dennnn364@gmail.com>
2023-02-28 19:51:31 +0200

    My second commit

---
+++ lab_2_StudentDemchuk.py
@@ -0,0 +1,34 @@
+# Перше завдання
+print("1.\n")
+
+print("Programming", "Essential", "in", sep="***", end="...")
+print("Python")
+
+# Друге завдання
+print("\n2.\n")
+
+print("    *\n   * *\n  *   *\n *     *")
+print("***","***", sep="   ")
+print("  *","*", sep="   ")
+print("  *","*", sep="   ")
+print("  *****")
+
+# Третє завдання
+print("\n3.\n")
+
+print("I'm student")
+
+# Четверте завдання
+print("\n4.\n")
+
+print('\"I\'m\"\n\""learning\""\n\"""Python\"""')
+
+# П'яте завдання
+print("\n5.\n")
+
+print(0o500)
+
+# Шосте завдання
+print("\n6.\n")
+
+print(0x777)
