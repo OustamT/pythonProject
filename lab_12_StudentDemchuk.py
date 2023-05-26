51e13223c0531697d2d80c5131a90b730fe2257e
Денис <demchuk.dennnn364@gmail.com>
2023-05-26 09:42:53 +0300

    add 12 lab

--- lab_12_StudentDemchuk.py
+++ lab_12_StudentDemchuk.py
@@ -3,8 +3,14 @@ import math
 
 class Point:
     def __init__(self, x=0.0, y=0.0):
-        self.__x = x
-        self.__y = y
+        self._x = x
+        self._y = y
+
+    def getx(self):
+        return self._x
+
+    def gety(self):
+        return self._y
 
 
 class Triangle:
@@ -12,9 +18,15 @@ class Triangle:
         self.__allVertice = [vertice1, vertice2, vertice3]
 
     def perimeter(self):
-        #
-        # __allVertice[0]
-        #
+        distancesA_B = math.hypot(self.__allVertice[0].getx() - self.__allVertice[1].getx(),
+                                  self.__allVertice[0].gety() - self.__allVertice[1].gety())
+        distancesB_C = math.hypot(self.__allVertice[1].getx() - self.__allVertice[2].getx(),
+                                  self.__allVertice[1].gety() - self.__allVertice[2].gety())
+        distancesC_A = math.hypot(self.__allVertice[2].getx() - self.__allVertice[0].getx(),
+                                  self.__allVertice[2].gety() - self.__allVertice[0].gety())
+
+        perimeter = distancesA_B + distancesB_C + distancesC_A
+        return perimeter
 
 
 triangle = Triangle(Point(0, 0), Point(1, 0), Point(0, 1))
